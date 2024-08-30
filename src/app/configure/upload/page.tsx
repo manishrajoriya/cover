fa79433ec7dfffb276190d38bf106d1b45a3ab1c
manish <gkrajoriya3@gmail.com>
2024-08-30 09:44:03 +0530

    landing page

---
+++ src/app/configure/upload/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { cn } from '@/lib/utils'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className={cn("relative h-full flex-1 my-16 w-full rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:rounded-2xl flex justify-center flex-col items-center")}>
+        
+    </div>
+  )
+}
+
+export default page
